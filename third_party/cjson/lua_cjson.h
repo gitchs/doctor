16d6b4e81e0f99e7fe75d800ba24bcafd20e6449
tinyproxy <tinyproxy@gmail.com>
2022-05-21 22:53:38 +0800

    add dep cjson

---
+++ third_party/cjson/lua_cjson.h
@@ -0,0 +1,16 @@
+/*
+ * lua_cjson.h
+ * Copyright (C) 2022 archlinux <archlinux@arch>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef __LUA_CJSON_H__
+#define __LUA_CJSON_H__
+
+#include "lua.h"
+
+int luaopen_cjson(lua_State* L);
+
+#endif // __LUA_CJSON_H__
+
