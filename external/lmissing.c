a3bffdf049ff1fe27f771aecac901c690dfc781b
tinyproxy <tinyproxy@gmail.com>
2022-05-23 17:40:03 +0800

    add strptime API

--- external/lmissing.c
+++ external/lmissing.c
@@ -5,8 +5,10 @@
  * Distributed under terms of the MIT license.
  */
 #include <errno.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 
 #include "lauxlib.h"
 #include "lmissing.h"
@@ -20,7 +22,26 @@ static int lmissing_mkdir(lua_State* L) {
   return 1;
 }
 
-const static luaL_Reg libs[] = {{"mkdir", lmissing_mkdir}, {NULL, NULL}};
+static int lmissing_strptime(lua_State* L) {
+  size_t dtlen = 0;
+  size_t flen = 0;
+  const char* const dt = lua_tolstring(L, 1, &dtlen);
+  const char* const dtformat = lua_tolstring(L, 2, &flen);
+  if (dt == NULL || dtformat == NULL) {
+    lua_pushnil(L);
+    return 1;
+  }
+  struct tm t;
+  bzero(&t, sizeof(struct tm));
+  strptime(dt, dtformat, &t);
+  time_t ts = mktime(&t);
+  lua_pushinteger(L, ts);
+  return 1;
+}
+
+const static luaL_Reg libs[] = {{"mkdir", lmissing_mkdir},
+                                {"strptime", lmissing_strptime},
+                                {NULL, NULL}};
 
 int luaopen_missing(lua_State* L) {
   luaL_newlib(L, libs);
