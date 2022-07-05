6b2bf110fa1d8a4d9f62d11993ed78cb337d4a4c
tinyproxy <tinyproxy@gmail.com>
2022-07-05 12:48:45 +0800

    bypass 10 max attach databases limit

--- app/mergedb.py
+++ app/mergedb.py
@@ -15,16 +15,22 @@ def main():
     cli_configure = cli_parser.parse_args()
     db = sqlite3.connect(cli_configure.output)
     select_sqls = []
-    for index in range(len(cli_configure.db)):
-        db_name = 'db%02d' % index
-        db_filename = cli_configure.db[index]
-        attach_sql = f"ATTACH DATABASE '{db_filename}' AS {db_name}"
-        select_sql = f"SELECT * FROM {db_name}.m2"
-        select_sqls.append(select_sql)
+    detach_sql = f"DETACH DATABASE src_db"
+    if len(cli_configure.db) > 1:
+        db_filename = cli_configure.db[0]
+        attach_sql = f"ATTACH DATABASE '{db_filename}' AS src_db"
+        ctas_sql = f"CREATE TABLE m2 AS SELECT * FROM src_db.m2"
         db.execute(attach_sql)
-    ctas_sql = "CREATE TABLE m2 AS %s" % ('\nUNION ALL\n'.join(select_sqls))
-    db.execute(ctas_sql)
-    db.commit()
+        db.execute(ctas_sql)
+        db.commit()
+        db.execute(detach_sql)
+    for db_filename in cli_configure.db[1:]:
+        attach_sql = f"ATTACH DATABASE '{db_filename}' AS src_db"
+        insert_sql = f"INSERT INTO m2 SELECT * FROM src_db.m2"
+        db.execute(attach_sql)
+        db.execute(insert_sql)
+        db.commit()
+        db.execute(detach_sql)
     db.execute("CREATE INDEX m2_query_id ON m2(query_id)")
     db.execute("CREATE INDEX m2_query_id_is_slow ON m2(query_id, is_slow)")
     db.execute("CREATE INDEX m2_query_id_is_slow_start_time ON m2(query_id, is_slow, start_time)")
