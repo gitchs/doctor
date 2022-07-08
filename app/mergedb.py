f9f5e2a9decbc548bf1082555000e2ff7311cc14
tinyproxy <tinyproxy@gmail.com>
2022-07-08 11:56:53 +0800

    refactor

--- app/mergedb.py
+++ app/mergedb.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python3
+"""
+parallel.py会生成很多sqlite-$PID.db数据库，
+为了方便分析,需要将他们合并
+"""
 import logging
 from argparse import ArgumentParser
 import sqlite3
-from pathlib import Path
+
 
 def main():
+    "main entry"
     logging.basicConfig(
         level=logging.INFO,
-        format='[%(asctime)s] %(filename)s:%(lineno)d %(message)s'                            # easy format
+        format='[%(asctime)s] %(filename)s:%(lineno)d %(message)s'
     )
     cli_parser = ArgumentParser()
     cli_parser.add_argument('--db', nargs='+', required=True)
     cli_parser.add_argument('--output', '-o', default='profile-merged.db')
     cli_configure = cli_parser.parse_args()
     db = sqlite3.connect(cli_configure.output)
-    select_sqls = []
-    detach_sql = f"DETACH DATABASE src_db"
+    detach_sql = "DETACH DATABASE src_db"
     if len(cli_configure.db) > 1:
         db_filename = cli_configure.db[0]
         attach_sql = f"ATTACH DATABASE '{db_filename}' AS src_db"
-        ctas_sql = f"CREATE TABLE m2 AS SELECT * FROM src_db.m2"
+        ctas_sql = "CREATE TABLE m2 AS SELECT * FROM src_db.m2"
         db.execute(attach_sql)
         db.execute(ctas_sql)
         db.commit()
         db.execute(detach_sql)
     for db_filename in cli_configure.db[1:]:
         attach_sql = f"ATTACH DATABASE '{db_filename}' AS src_db"
-        insert_sql = f"INSERT INTO m2 SELECT * FROM src_db.m2"
+        insert_sql = "INSERT INTO m2 SELECT * FROM src_db.m2"
         db.execute(attach_sql)
         db.execute(insert_sql)
         db.commit()
         db.execute(detach_sql)
     db.execute("CREATE INDEX m2_query_id ON m2(query_id)")
     db.execute("CREATE INDEX m2_query_id_is_slow ON m2(query_id, is_slow)")
-    db.execute("CREATE INDEX m2_query_id_is_slow_start_time ON m2(query_id, is_slow, start_time)")
+    db.execute("""
+CREATE INDEX m2_query_id_is_slow_start_time
+ON m2(query_id, is_slow, start_time)""")
     db.execute('''CREATE VIEW mview AS
 SELECT query_id
 ,query_type
---
+++ app/parallel.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""test.lua是单线程，并没有提供任何并发的能力。
+为了提高吞吐，使用python包一层，通过多进程分发文件处理
+"""
+from argparse import ArgumentParser
+import logging
+from pathlib import Path
+from multiprocessing.pool import ThreadPool
+from subprocess import Popen
+
+
+def task(f: Path) -> int:
+    "开子进程，处理任务"
+    f = f.absolute()
+    logging.info('handle file "%s"', f)
+    child_args = [
+        str(Path(__file__).parent.joinpath('doctor').absolute()),
+        str(Path(__file__).parent.joinpath('test.lua').absolute()),
+        str(f),
+    ]
+    with Popen(child_args, cwd=str(Path(__file__).parent.absolute())) as child:
+        return child.wait()
+
+
+def fit(dirs):
+    "文件任务队列iterator"
+    for d in dirs:
+        for f in Path(d).glob('*.avro'):
+            yield f
+
+def main():
+    "main entry"
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(asctime)s] %(filename)s:%(lineno)d %(message)s'
+    )
+    cli_parser = ArgumentParser()
+    cli_parser.add_argument('--root', '-r', nargs='+', required=True)
+    cli_parser.add_argument('--bin', required=False, default='./doctor')
+    cli_parser.add_argument('--jobs', '-j', default='6', type=int, required=False)
+    cli_configure = cli_parser.parse_args()
+    with ThreadPool(cli_configure.jobs) as p:
+        p.map(task, fit(cli_configure.root))
+
+if __name__ == '__main__':
+    main()
